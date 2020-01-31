2a2becce94b68e8ce988a4fb886ff9e709c3f670
Heena Bhansali <heenaj198@gmail.com>
2020-01-31 14:59:15 +0530

    add routes with dynamic segments

--- index.js
+++ index.js
@@ -8,14 +8,35 @@ const getUsersRoute = (req, res) => {
   res.send(users)
 }
 
+const getUserRoute = (req, res) => {
+  const user = users.find(user => user.id === req.params.id)
+  res.send(user)
+}
+
 const getEmailsRoute = (req, res) => {
   res.send(emails)
+}
+
+const getEmailRoute = (req, res) => {
+  const email = emails.find(email => email.id === req.params.id)
+  res.send(email)
+}
+
+const getEmailFrom = (req, res) => {
+  const email = emails.find(
+    email =>
+      email.from === req.params.sender && email.to === req.params.recipient
+  )
+  res.send(email)
 }
 
 const router = express.Router()
 
 router.get("/users", getUsersRoute)
+router.get("/users/:id", getUserRoute)
 router.get("/emails", getEmailsRoute)
+router.get("/emails/:id", getEmailRoute)
+router.get("/emails/from/:sender/to/:recipient", getEmailFrom)
 
 app.use(router)
 
